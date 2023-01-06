138017c901e942214fa74909b216a06e65454dd1
Mrjiks <jiksdevops@gmail.com>
2023-01-06 00:43:52 -0800

    Animate mobile menu

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,179 +1,170 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdOutlineLocationOn } from 'react-icons/md';
+import React from "react";
+import styled from "styled-components";
+import { MdOutlineLocationOn } from "react-icons/md";
 
 const HeroSection = () => {
-	return (
-		<StyledHeroWrapper>
-			<div className='wrapper'>
-				<div className='container'>
-					<h1 className='hero-text'>Find the apartment of your dreams.</h1>
-					<p className='hero-more-info'>
-						See verified property listings with <span>100%</span> transparency.
-					</p>
-					<div className='cta-container'>
-						<button
-							type='button'
-							className='buy-btn'
-						>
-							Buy
-						</button>
-						<p
-							type='button'
-							className='rent-btn'
-						>
-							Rent
-						</p>
-					</div>
-					<div className='location-container'>
-						<MdOutlineLocationOn className='location-logo' />
-						<p
-							type='button'
-							className='location-btn'
-						>
-							Property location
-						</p>
-					</div>
-				</div>
-				<img
-					className='hero-img'
-					src='http://cdn.home-designing.com/wp-content/uploads/2017/05/wood-white-and-charcoal-modern-exterior-paint-themes.jpg'
-					alt='house'
-				/>
-			</div>
-		</StyledHeroWrapper>
-	);
+  return (
+    <StyledHeroWrapper>
+      <div className="wrapper">
+        <div className="container">
+          <h1 className="hero-text">Find the apartment of your dreams.</h1>
+          <p className="hero-more-info">
+            See verified property listings with <span>100%</span> transparency.
+          </p>
+          <div className="cta-container">
+            <button type="button" className="buy-btn">
+              Buy
+            </button>
+            <p type="button" className="rent-btn">
+              Rent
+            </p>
+          </div>
+          <div className="location-container">
+            <MdOutlineLocationOn className="location-logo" />
+            <p type="button" className="location-btn">
+              Property location
+            </p>
+          </div>
+        </div>
+        <img
+          className="hero-img"
+          src="http://cdn.home-designing.com/wp-content/uploads/2017/05/wood-white-and-charcoal-modern-exterior-paint-themes.jpg"
+          alt="house"
+        />
+      </div>
+    </StyledHeroWrapper>
+  );
 };
 
 export default HeroSection;
 
 const StyledHeroWrapper = styled.main`
-	display: flex;
-	flex-direction: column;
-	flex-basis: 1;
-	justify-content: space-between;
-	align-items: center;
-	background-color: #f0fcfa;
+  display: flex;
+  flex-direction: column;
+  flex-basis: 1;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f0fcfa;
 
-	.container {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
 
-		h1,
-		p {
-			text-align: left;
-			margin: 10px;
-			letter-spacing: -0.02em;
-		}
-		p {
-			color: black;
-			text-align: left;
-		}
-		span {
-			color: olivedrab;
-		}
-		.cta-container {
-			background-color: white;
-			display: flex;
-			padding: 1rem;
-			width: 70%;
-			height: 62px;
-			border-radius: 8px;
-			align-items: center;
-			margin: 15px;
-		}
-		.buy-btn {
-			width: 60%;
-			height: 38px;
-			border-radius: 8px;
-			background-color: olivedrab;
-			color: white;
-			margin-right: 20px;
-		}
+    h1,
+    p {
+      text-align: left;
+      margin: 10px;
+      letter-spacing: -0.02em;
+    }
+    p {
+      color: black;
+      text-align: left;
+    }
+    span {
+      color: olivedrab;
+    }
+    .cta-container {
+      background-color: white;
+      display: flex;
+      padding: 1rem;
+      width: 70%;
+      height: 62px;
+      border-radius: 8px;
+      align-items: center;
+      margin: 15px;
+    }
+    .buy-btn {
+      width: 60%;
+      height: 38px;
+      border-radius: 8px;
+      background-color: olivedrab;
+      color: white;
+      margin-right: 20px;
+    }
 
-		.rent-btn {
-			width: 60px;
-			color: black;
-			margin-right: 20px;
-			background-color: white;
-		}
+    .rent-btn {
+      width: 60px;
+      color: black;
+      margin-right: 20px;
+      background-color: white;
+    }
 
-		.location-container {
-			background-color: white;
-			display: flex;
-			padding: 1rem;
-			width: 90%;
-			height: 54px;
-			border-radius: 8px;
-			align-items: center;
-			margin: 15px;
-			.location-btn {
-				width: 70%;
-				color: black;
-				margin-right: 10px;
-				background-color: white;
-			}
-			.location-logo {
-				font-size: 20px;
-				color: olivedrab;
-				margin-right: 2rem;
-			}
-		}
-	}
+    .location-container {
+      background-color: white;
+      display: flex;
+      padding: 1rem;
+      width: 90%;
+      height: 54px;
+      border-radius: 8px;
+      align-items: center;
+      margin: 15px;
+      .location-btn {
+        width: 70%;
+        color: black;
+        margin-right: 10px;
+        background-color: white;
+      }
+      .location-logo {
+        font-size: 20px;
+        color: olivedrab;
+        margin-right: 2rem;
+      }
+    }
+  }
 
-	img {
-		display: block;
-		max-width: 100%;
-		object-fit: cover;
-	}
+  img {
+    display: block;
+    max-width: 100%;
+    object-fit: cover;
+  }
 
-	@media (min-width: 768px) {
-		.wrapper {
-			display: flex;
-			flex-direction: row;
-			flex-basis: 1;
-		}
-		.container {
-			display: flex;
-			flex-direction: column;
-			width: 95%;
-			margin: 0 auto;
-			h1,
-			p {
-				text-align: left;
-				margin: 10px;
-			}
-			p {
-				color: black;
-				text-align: left;
-			}
-			span {
-				color: olivedrab;
-			}
-			.cta-container {
-				background-color: white;
-				display: flex;
-				padding: 1rem;
-				width: 60%;
-				border-radius: 8px;
-			}
-			.buy-btn {
-				width: 60px;
-				height: 25px;
-				border-radius: 8px;
-				background-color: olivedrab;
-				color: white;
-				margin-right: 20px;
-			}
-		}
-		img {
-			display: block;
-			max-width: 100%;
-			object-fit: cover;
-		}
-		.hero-img {
-			width: 55%;
-		}
-	}
+  @media (min-width: 768px) {
+    .wrapper {
+      display: flex;
+      flex-direction: row;
+      flex-basis: 1;
+    }
+    .container {
+      display: flex;
+      flex-direction: column;
+      width: 95%;
+      margin: 0 auto;
+      h1,
+      p {
+        text-align: left;
+        margin: 10px;
+      }
+      p {
+        color: black;
+        text-align: left;
+      }
+      span {
+        color: olivedrab;
+      }
+      .cta-container {
+        background-color: white;
+        display: flex;
+        padding: 1rem;
+        width: 60%;
+        border-radius: 8px;
+      }
+      .buy-btn {
+        width: 60px;
+        height: 25px;
+        border-radius: 8px;
+        background-color: olivedrab;
+        color: white;
+        margin-right: 20px;
+      }
+    }
+    img {
+      display: block;
+      max-width: 100%;
+      object-fit: cover;
+    }
+    .hero-img {
+      width: 55%;
+    }
+  }
 `;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,32 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 import {
-	SearchProperty,
-	HeroSection,
-	NavBarWrapper,
-	FeaturedProperty,
-	MobileMenu,
-} from '../components';
+  SearchProperty,
+  HeroSection,
+  NavBarWrapper,
+  FeaturedProperty,
+  MobileMenu,
+} from "../components";
 
-import { useGlobalContext } from '../components/context';
+import { useGlobalContext } from "../components/context";
 
 function Landing() {
-	const { showMenu } = useGlobalContext();
+  const { showMenu } = useGlobalContext();
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
-	return (
-		<div>
-			<NavBarWrapper />
-			<HeroSection />
-			<SearchProperty />
-			<FeaturedProperty />
-			{showMenu && <MobileMenu />}
-		</div>
-	);
+  return (
+    <Wrapper>
+      <NavBarWrapper />
+      <HeroSection />
+      <SearchProperty />
+      <FeaturedProperty />
+      {showMenu && <MobileMenu />}
+      <div>New</div>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+    </Wrapper>
+  );
 }
 
 export default Landing;
 
 const Wrapper = styled.div`
-	display: flex;
+  display: flex;
+  flex-direction: column;
+  .progress-bar {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 10px;
+    background: var(--red);
+    transform-origin: 0%;
+  }
 `;
